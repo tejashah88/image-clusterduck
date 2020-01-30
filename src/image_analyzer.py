ed36c42fa0777363907aa6b96aed2c4715f9bcc1
Tejas Shah <tejashah88@gmail.com>
2020-01-30 02:20:21 -0800

    lots of renaming and trying out the loading cursor

--- src/image_analyzer.py
+++ src/image_analyzer.py
@@ -15,7 +15,7 @@ from plot_3d import Plot3D
 DEFAULT_IMG_FILENAME = './test-images/starry-night.jpg'
 SUPPORTED_IMG_EXTS = '*.png *.jpg *.jpeg *.gif *.bmp *.tiff *.tif'
 
-def make_img_scatterplot(cv_img, color_mode, scale_factor=3):
+def img_scatterplot(cv_img, color_mode, scale_factor=3):
     pos_arr = cv_img[color_mode].reshape(-1, 3) / 255 * scale_factor
     color_arr = cv_img.RGB.reshape(-1, 3) / 255
 
@@ -26,7 +26,7 @@ def make_img_scatterplot(cv_img, color_mode, scale_factor=3):
     )
 
 
-def make_pos_to_color_scatterplot(cv_img, color_mode, ch_index, scale_factor=3):
+def pos_color_scatterplot(cv_img, color_mode, ch_index, scale_factor=3):
     rgb_img = cv_img.RGB
     converted_img = cv_img[color_mode]
 
@@ -55,7 +55,6 @@ def setup_axes_links(leader_plot, follower_plots):
 # Interpret image data as row-major instead of col-major
 pg.setConfigOptions(imageAxisOrder='row-major')
 
-
 class MyWindow(pg.GraphicsLayoutWidget):
     def __init__(self):
         super().__init__()
@@ -69,6 +68,11 @@ class MyWindow(pg.GraphicsLayoutWidget):
         self.glvw_color_vis = None
         self.channel_plot = None
         self.glvw_channel_vis = None
+
+        self.menubar = None
+        self.statusbar = None
+
+        self.loading = False
 
 
     @property
@@ -87,29 +91,44 @@ class MyWindow(pg.GraphicsLayoutWidget):
         return COLOR_SPACE_LABELS[self.color_mode][self.ch_index]
 
 
+    @property
+    def curr_image(self):
+        return self.cv_img[self.color_mode]
+
+
+    @property
+    def curr_image_slice(self):
+        return self.cv_img[self.color_mode][:, :, self.ch_index]
+
+
     def load_image(self, img_path, max_pixels=1000000):
-        input_img = cv2.imread(img_path)
+        with pg.BusyCursor():
+            self.is_loading(True)
 
-        if input_img is None:
-            print(f'Error: Unable to load image from {img_path}')
-            exit(-1)
+            input_img = cv2.imread(img_path)
 
-        height, width = input_img.shape[:2]
-        num_pixels = width * height
-        if num_pixels > max_pixels:
-            resize_factor = 1 / ( (num_pixels / max_pixels) ** 0.5 )
-            print('Resize factor:', resize_factor)
-            input_img = cv2.resize(input_img, None, fx=resize_factor, fy=resize_factor)
+            if input_img is None:
+                print(f'Error: Unable to load image from {img_path}')
+                exit(-1)
 
-        self.cv_img = CvImg.from_ndarray(input_img)
+            height, width = input_img.shape[:2]
+            num_pixels = width * height
+            if num_pixels > max_pixels:
+                resize_factor = 1 / ( (num_pixels / max_pixels) ** 0.5 )
+                print('Resize factor:', resize_factor)
+                input_img = cv2.resize(input_img, None, fx=resize_factor, fy=resize_factor)
 
-        print('Original number of pixels:', num_pixels)
+            self.cv_img = CvImg.from_ndarray(input_img)
 
-        if self.gui_ready:
-            self.orig_img_plot.set_image(self.cv_img.RGB)
-            self.glvw_color_vis.set_plot(make_img_scatterplot(self.cv_img, self.color_mode))
-            self.on_color_space_change(self.cs_index)
-            self.on_channel_view_change(self.ch_index)
+            print('Original number of pixels:', num_pixels)
+
+            if self.gui_ready:
+                self.orig_img_plot.set_image(self.cv_img.RGB)
+                self.glvw_color_vis.set_plot(img_scatterplot(self.cv_img, self.color_mode))
+                self.on_color_space_change(self.cs_index)
+                self.on_channel_view_change(self.ch_index)
+
+        self.is_loading(False)
 
 
     def setup_gui(self):
@@ -117,11 +136,10 @@ class MyWindow(pg.GraphicsLayoutWidget):
             raise Exception('Error: Image has not been loaded yet! Please call load_image() before calling setup_gui()')
 
         self.orig_img_plot = ImagePlotter(title='Original Image', img=self.cv_img.RGB, enable_roi=True)
-        self.glvw_color_vis = Plot3D(plot=make_img_scatterplot(self.cv_img, self.color_mode))
+        self.glvw_color_vis = Plot3D(plot=img_scatterplot(self.cv_img, self.color_mode))
 
-        channel_isolated_img = self.cv_img[self.color_mode][:, :, self.ch_index]
-        self.channel_plot = ImagePlotter(title=self.channel_mode, img=channel_isolated_img)
-        self.glvw_channel_vis = Plot3D(plot=make_pos_to_color_scatterplot(self.cv_img, self.color_mode, self.ch_index))
+        self.channel_plot = ImagePlotter(title=self.channel_mode, img=self.curr_image_slice)
+        self.glvw_channel_vis = Plot3D(plot=pos_color_scatterplot(self.cv_img, self.color_mode, self.ch_index))
 
         setup_axes_links(self.orig_img_plot, [self.channel_plot])
 
@@ -149,13 +167,22 @@ class MyWindow(pg.GraphicsLayoutWidget):
 
         # Set the layout and resize the window accordingly
         self.setLayout(grid_layout)
-        self.resize(grid_layout.sizeHint() + QtCore.QSize(10, 10))
+        self.resize(grid_layout.sizeHint() + QtCore.QSize(10, 30))
+
+
+    def is_loading(self, val):
+        self.loading = val
+        if self.loading:
+            self.setWindowModality(QtCore.Qt.ApplicationModal)
+        else:
+            self.setWindowModality(QtCore.Qt.NonModal)
 
 
     def bind_to_main_window(self, main_window):
         main_window.setCentralWidget(self)
 
         self.setup_menubar(main_window)
+        self.setup_statusbar(main_window)
 
         main_window.resize(self.size())
 
@@ -173,12 +200,12 @@ class MyWindow(pg.GraphicsLayoutWidget):
     def on_color_space_change(self, cspace_index):
         self.cs_index = cspace_index
 
-        self.glvw_color_vis.set_plot(plot=make_img_scatterplot(self.cv_img, self.color_mode))
+        self.glvw_color_vis.set_plot(plot=img_scatterplot(self.cv_img, self.color_mode))
 
         self.channel_plot.setTitle(title=self.channel_mode)
-        self.channel_plot.set_image(img=self.cv_img[self.color_mode][:, :, self.ch_index])
+        self.channel_plot.set_image(img=self.curr_image_slice)
 
-        self.glvw_channel_vis.set_plot(plot=make_pos_to_color_scatterplot(self.cv_img, self.color_mode, self.ch_index))
+        self.glvw_channel_vis.set_plot(plot=pos_color_scatterplot(self.cv_img, self.color_mode, self.ch_index))
 
         self.channel_cbox.clear()
         self.channel_cbox.addItems(COLOR_SPACE_LABELS[self.color_mode])
@@ -196,34 +223,39 @@ class MyWindow(pg.GraphicsLayoutWidget):
         self.channel_plot.set_image(new_img)
 
         # Update the scatterplot
-        new_scatter = make_pos_to_color_scatterplot(self.cv_img, self.color_mode, ch_index)
+        new_scatter = pos_color_scatterplot(self.cv_img, self.color_mode, ch_index)
         self.glvw_channel_vis.plt_item.setData(pos=new_scatter.pos, color=new_scatter.color)
 
 
     def setup_menubar(self, main_window):
-        menubar = main_window.menuBar()
-        fileMenu = menubar.addMenu('File')
-        editMenu = menubar.addMenu('Edit')
-        viewMenu = menubar.addMenu('View')
-        searchMenu = menubar.addMenu('Search')
-        toolsMenu = menubar.addMenu('Tools')
-        helpMenu = menubar.addMenu('Help')
+        self.menubar = main_window.menuBar()
+        file_menu = self.menubar.addMenu('File')
+        edit_menu = self.menubar.addMenu('Edit')
+        view_menu = self.menubar.addMenu('View')
+        search_menu = self.menubar.addMenu('Search')
+        tools_menu = self.menubar.addMenu('Tools')
+        help_menu = self.menubar.addMenu('Help')
 
-        openImageAction = QtGui.QAction('Open Image', self)
-        openImageAction.setShortcut('Ctrl+O')
-        openImageAction.setStatusTip('Open Image')
-        openImageAction.triggered.connect(lambda: self.load_image(self.open_image_file_dialog()))
-        fileMenu.addAction(openImageAction)
+        open_image_action = QtGui.QAction('Open Image', self)
+        open_image_action.setShortcut('Ctrl+O')
+        open_image_action.setStatusTip('Open Image')
+        open_image_action.triggered.connect(lambda: self.load_image(self.open_image_file_dialog()))
+        file_menu.addAction(open_image_action)
 
-        exitAction = QtGui.QAction('Exit', self)
-        exitAction.setShortcut('Ctrl+Q')
-        exitAction.setStatusTip('Exit application')
-        exitAction.triggered.connect(main_window.close)
-        fileMenu.addAction(exitAction)
+        exit_action = QtGui.QAction('Exit', self)
+        exit_action.setShortcut('Ctrl+Q')
+        exit_action.setStatusTip('Exit application')
+        exit_action.triggered.connect(main_window.close)
+        file_menu.addAction(exit_action)
 
 
     def setup_statusbar(self, main_window):
-        statusbar = main_window.statusBar()
+        self.statusbar = main_window.statusBar()
+
+
+    def show_status(self, text, timeout=0):
+        self.statusbar.showMessage(text, timeout)
+
 
 ## Start Qt event loop unless running in interactive mode.
 if __name__ == '__main__':
